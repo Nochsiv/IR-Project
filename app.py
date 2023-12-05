9a681d6d6ff0c306536f09a4e0d57aebd76286f3
Visal-Som30 <visal030303@gmail.com>
2023-12-05 23:07:16 +0700

    Final

---
+++ app.py
@@ -0,0 +1,241 @@
+# import streamlit as st
+# from PIL import Image
+# import pandas as pd
+# from bert_search import SingletonSearch
+# from okapi_bm25_search import OkapiBM25Search
+
+# bert_search = SingletonSearch()
+# okapi_search = OkapiBM25Search()
+
+# # Load the CSV file into a DataFrame
+# df = pd.read_csv("datasets/model_rating.csv")
+
+# # update the number of satisfaction of each model
+# def update_model_satisfaction(df, model_name,type):
+#     index = df.index[df["model_name"] == model_name].tolist()[0]
+#     if type == 'increase':
+#         df.at[index, "num_of_rating"] += 1
+#     else:
+#         df.at[index, "num_of_rating"] -= 1
+
+#     # Save the updated DataFrame back to the CSV file
+#     df.to_csv("datasets/model_rating.csv", index=False)
+
+
+# def display_movies(data):
+#     st.write("Display movies")
+#     if data is None:
+#         st.info("No News")
+#     else:
+#         for i in range(len(data)):
+#             story = data.iloc[i]
+#             title = story["title"]
+#             image = Image.open("assets/netflix.png")
+        
+#             if title is not None:
+#                 col1, col2 = st.columns([1, 3])
+#                 with col1:
+#                     if image is not None: st.image(image,width=150)
+#                 with col2:
+#                     st.markdown('<style>.title {font-weight: bold;font-size:20px}</style>', unsafe_allow_html=True)
+#                     st.markdown(f'<p class="title">{title}</p>', unsafe_allow_html=True)
+#                     st.markdown(f'<p >{story["description"]}</p>', unsafe_allow_html=True)
+#                     st.write(f"Durations: {story['duration']}, Realease Year: {story['release_year']}&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Relevant Score: {round(story['score'],5)}")
+# def sidebar():
+#     with st.sidebar:
+#         st.markdown('<style>.rate {font-weight: bold;font-size:20px;color:red}</style>', unsafe_allow_html=True)
+#         st.markdown(f'<p>Model : <span class="rate">BERT</span> => Number of Satisfaction:<span class="rate"> {df.iloc[0]["num_of_rating"]}</span></p>', unsafe_allow_html=True)
+#         st.markdown(f'<p>Model : <span class="rate">Okapi BM25</span> => Number of Satisfaction:<span class="rate"> {df.iloc[1]["num_of_rating"]}</span></p>', unsafe_allow_html=True)
+
+# def main():
+#     st.set_page_config(
+#         page_title="Netflix Movie Search Engine",
+#         page_icon="🎥",
+#         initial_sidebar_state="expanded",
+#         menu_items={"About": "Built by @dcarpintero with Streamlit & NewsAPI"},
+#     )
+
+#     st.title(">> Netflix Movies Search Engine <<")
+#     # Add input elements
+#     # Create a text input for the user to enter the search query
+#     search_query = st.text_input("Enter the Movie's Title:")
+
+#     option = st.radio(
+#         label="Choose a Model:",
+#         options=("BERT", "Okapi BM25")
+#     )
+
+#     data, duration = bert_search.search(search_query) if option == "BERT" else okapi_search.search(search_query)
+
+#     if not duration == 0:
+#         st.markdown('<style>.duration {font-weight: bold}</style>', unsafe_allow_html=True)
+#         st.markdown(f'<p>Results after <span class="duration">{round((duration/1000),3)}</span> seconds</p>', unsafe_allow_html=True)
+#     col1, col2, col3 = st.columns([4,1,1])
+
+#     # Initialize session state
+#     if "like_button" not in st.session_state:
+#         st.session_state.like_button = False
+#     if "dislike_button" not in st.session_state:
+#         st.session_state.dislike_button = False
+
+#     with col1:
+#         st.markdown(f"Do you satisfy with the results using this Model ({option})?")
+#     with col2:
+#         # Display the button
+#         like_button = st.button("Like 👍")
+
+#         # Check if the button has not been clicked before
+#         if like_button and not st.session_state.like_button:
+#             if option == "BERT":
+#                 update_model_satisfaction(df,"bert","increase")
+#             else:
+#                 update_model_satisfaction(df,"okapi","increase")
+#             # Set button_clicked to True to indicate that the button has been clicked
+#             st.session_state.like_button = True
+#             # st.session_state.dislike_button = True
+            
+#     with col3:
+#         # Display the button
+#         dislike_button = st.button("Dislike 👍")
+
+#         # Check if the button has not been clicked before
+#         if dislike_button and not st.session_state.dislike_button:
+#             if option == "BERT":
+#                 update_model_satisfaction(df,"bert","decrease")
+#             else:
+#                 update_model_satisfaction(df,"okapi","decrease")
+#             # Set button_clicked to True to indicate that the button has been clicked
+#             st.session_state.dislike_button = True
+#             # st.session_state.like_button = True
+            
+#     sidebar()
+#     display_movies(data)
+
+# if __name__ == "__main__":
+#     main()
+
+
+import streamlit as st
+from PIL import Image
+import pandas as pd
+from bert_search import SingletonSearch
+from okapi_bm25_search import OkapiBM25Search
+
+bert_search = SingletonSearch()
+okapi_search = OkapiBM25Search()
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv("datasets/model_rating.csv")
+
+# Update the number of satisfaction of each model
+def update_model_satisfaction(df, model_name, type):
+    index = df.index[df["model_name"] == model_name].tolist()[0]
+    if type == 'increase':
+        df.at[index, "num_of_rating"] += 1
+    else:
+        df.at[index, "num_of_rating"] -= 1
+
+    # Save the updated DataFrame back to the CSV file
+    df.to_csv("datasets/model_rating.csv", index=False)
+
+def display_movies(data):
+    st.write("Display movies")
+    if data is None:
+        st.info("No News")
+    else:
+        for i in range(len(data)):
+            story = data.iloc[i]
+            title = story["title"]
+            image = Image.open("assets/netflix.png")
+
+            if title is not None:
+                col1, col2 = st.columns([1, 3])
+                with col1:
+                    if image is not None: st.image(image, width=150)
+                with col2:
+                    st.markdown('<style>.title {font-weight: bold;font-size:20px}</style>', unsafe_allow_html=True)
+                    st.markdown(f'<p class="title">{title}</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p >{story["description"]}</p>', unsafe_allow_html=True)
+                    st.write(f"Durations: {story['duration']}, Realease Year: {story['release_year']}&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Relevant Score: {round(story['score'],5)}")
+
+def sidebar():
+    with st.sidebar:
+        st.markdown('<style>.rate {font-weight: bold;font-size:20px;color:red}</style>', unsafe_allow_html=True)
+        st.markdown(
+            f'<p>Model : <span class="rate">BERT</span> => Number of Satisfaction:<span class="rate"> {df.iloc[0]["num_of_rating"]}</span></p>',
+            unsafe_allow_html=True)
+        st.markdown(
+            f'<p>Model : <span class="rate">Okapi BM25</span> => Number of Satisfaction:<span class="rate"> {df.iloc[1]["num_of_rating"]}</span></p>',
+            unsafe_allow_html=True)
+
+def main():
+    st.set_page_config(
+        page_title="Netflix Movie Search Engine",
+        page_icon="🎥",
+        initial_sidebar_state="expanded",
+        menu_items={"About": "Built by @dcarpintero with Streamlit & NewsAPI"},
+    )
+
+    st.title(">> Netflix Movies Search Engine <<")
+    # Add input elements
+    # Create a text input for the user to enter the search query
+    search_query = st.text_input("Enter the Movie's Title:")
+
+    option = st.radio(
+        label="Choose a Model:",
+        options=("BERT", "Okapi BM25")
+    )
+
+    data, duration = bert_search.search(search_query) if option == "BERT" else okapi_search.search(search_query)
+
+    if not duration == 0:
+        st.markdown('<style>.duration {font-weight: bold}</style>', unsafe_allow_html=True)
+        st.markdown(
+            f'<p>Results after <span class="duration">{round((duration / 1000), 3)}</span> seconds</p>',
+            unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([4, 1, 1])
+
+    # Initialize session state
+    if "like_button" not in st.session_state:
+        st.session_state.like_button = False
+    if "dislike_button" not in st.session_state:
+        st.session_state.dislike_button = False
+    if "search_query" not in st.session_state:
+        st.session_state.search_query = ""
+
+    with col1:
+        st.markdown(f"Do you satisfy with the results using this Model ({option})?")
+    with col2:
+        # Display the button
+        like_button = st.button("Like 👍")
+
+        # Check if the button has not been clicked before
+        if like_button and not st.session_state.like_button:
+            if option == "BERT":
+                update_model_satisfaction(df, "bert", "increase")
+            else:
+                update_model_satisfaction(df, "okapi", "increase")
+            # Set button_clicked to True to indicate that the button has been clicked
+            st.session_state.like_button = True
+
+    with col3:
+        # Display the button
+        dislike_button = st.button("Dislike 👍")
+
+        # Check if the button has not been clicked before
+        if dislike_button and not st.session_state.dislike_button:
+            if option == "BERT":
+                update_model_satisfaction(df, "bert", "decrease")
+            else:
+                update_model_satisfaction(df, "okapi", "decrease")
+            # Set button_clicked to True to indicate that the button has been clicked
+            st.session_state.dislike_button = True
+
+    # Update the stored search query in session state
+    st.session_state.search_query = search_query
+
+    sidebar()
+    display_movies(data)
+
+if __name__ == "__main__":
+    main()
